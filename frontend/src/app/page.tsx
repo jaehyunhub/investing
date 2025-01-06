7f61ef95b78203eb03f64a5285ac7eb23c060224
hyun <jaehyoen3@gmail.com>
2025-01-07 00:14:04 +0900

    Main Page UI

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,101 +1,66 @@
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <main className="w-full min-h-screen bg-black">
+      <div className="relative w-full">
+        <div className="absolute top-8 left-1/2 transform -translate-x-1/2 z-10 text-white text-center">
+          <div className="flex items-center">
+          <Image
+              src="/images/newlogo.png"
+              alt="Sukaworld"
+              width={32}
+              height={32}
+              className="h-8 mx-auto "
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <span className="block mt-2 ml-3 text-4xl font-medium">슈카월드</span>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <div className="flex flex-col md:flex-row w-full min-h-screen">
+          <div className="relative w-full md:w-1/2 h-screen">
+            <div className="absolute inset-0 bg-black/20 z-10" />
+            <Image
+              src="/images/newsukaworld.png"
+              alt="Snowy mountain camping scene"
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              priority
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute inset-0 flex items-center justify-center z-20">
+              <div className="text-center text-white">
+                <h2 className="text-4xl md:text-5xl mb-2 font-medium">
+                  슈카월드
+                </h2>
+                <p className="text-xl md:text-2xl font-light">자세히보기</p>
+              </div>
+            </div>
+          </div>
+          <div className="relative w-full md:w-1/2 h-screen">
+            <div className="absolute inset-0 bg-black/20 z-10" />
+            <Image
+              src="/images/stockisnow.jpg"
+              alt="Mountain apparel scene"
+              fill
+              sizes="(max-width: 768px) 100vw, 50vw"
+              priority
+              className="object-cover w-full h-full"
+            />
+            <div className="absolute inset-0 flex items-center justify-center z-20">
+              <div className="text-center text-white">
+                <h2 className="text-4xl md:text-5xl font-medium mb-2">
+                 슈카코믹스
+                </h2>
+                <p className="text-xl md:text-2xl font-light">자세히 보기</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+         {/* Footer 추가 */}
+         <div className="absolute bottom-10 left-1/2 transform -translate-x-1/2 z-10">
+          <span className="text-white text-xl opacity-70">© Syuka Friends Inc</span>
+        </div>
+    </main>
   );
 }
